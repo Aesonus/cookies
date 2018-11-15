8c039c3a48956fa7d735ef7d58371fea5bd9c47f
Aesonus <corylcomposinger@gmail.com>
2018-11-15 09:36:16 -0600

    Updated phpdocs and dependencies

--- src/RequestCookiesBuilder.php
+++ src/RequestCookiesBuilder.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This file is part of the slenderman/authentication project.
+ * This file is part of the aesonus/cookies project.
  *
  * (c) Cory Laughlin <corylcomposinger@gmail.com>
  *
--- src/RequestCookiesBuilderInterface.php
+++ src/RequestCookiesBuilderInterface.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the slenderman/authentication project.
+ * This file is part of the aesonus/cookies project.
  *
  * (c) Cory Laughlin <corylcomposinger@gmail.com>
  *
